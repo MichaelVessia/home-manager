08d9ce931830899b44cdc4006349b4bc034a1149
Michael Vessia <michael@vessia.net>
2025-06-22 08:21:28 -0400

    Restructure config

--- home.nix
+++ home.nix
@@ -8,37 +8,19 @@ let
   }) {
     inherit pkgs;
   };
+  # Share username across various bits of the config
+  username = "michaelvessia";
 in
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "michaelvessia";
-  home.homeDirectory = "/home/michaelvessia";
+  nix = {
+    package = pkgs.nix;
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "25.05"; # Please read the comment before changing.
-
-  # Enable nixGL integration
-  nixGL.packages = nixgl;
-  
-  # Configure nixGL wrapper based on your GPU
-  # For Intel/AMD Mesa drivers:
-  nixGL.defaultWrapper = "mesa";
-  # For Nvidia proprietary drivers, uncomment the line below and comment out mesa:
-  # nixGL.defaultWrapper = "nvidia";
-  
-  # Install wrapper scripts
-  nixGL.installScripts = [ "mesa" ];  # Change to [ "nvidia" ] if using Nvidia
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home = {
+    inherit username;
+    homeDirectory = "/home/${username}";
+    stateVersion = "25.05"; # Don't change this
     packages = with pkgs; [
       vim
       git
@@ -52,26 +34,21 @@ in
       jetbrains-mono
       nerd-fonts.jetbrains-mono
       nerd-fonts.fira-code
-  ];
-};
+    ];
+    sessionVariables = {
+	    EDITOR = "vim";
+    };
+  };
+
+  nixGL = {
+    # Enable nixGL integration
+    packages = nixgl;
+    defaultWrapper = "mesa";
+    installScripts = [ "mesa" ];
+  };
 
   # Enable font configuration
   fonts.fontconfig.enable = true;
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
 
   # Configure Ghostty
   xdg.configFile."ghostty/config".text = ''
@@ -100,47 +77,17 @@ in
     shell-integration = detect
   '';
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/michaelvessia/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    EDITOR = "vim";
-    
-    # Uncomment these if you still have GL issues:
-    # LIBGL_ALWAYS_SOFTWARE = "1";  # Force software rendering
-    # GALLIUM_DRIVER = "llvmpipe";   # Use llvmpipe driver
-  };
-
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
   # Git config using Home Manager modules
   programs.git = {
     enable = true;
-    userName = "michaelvessia";
+    userName = username;
     userEmail = "michael@vessia.net";
     aliases = {
       st = "status";
     };
   };
-
-  nix = {
-	  package = pkgs.nix;
-	  settings.experimental-features = [ "nix-command" "flakes" ];
-  };
-
 
 }
