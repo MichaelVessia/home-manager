b65ee894d513288916ee4e5ddad0792d5eb620d0
Michael Vessia <michael@vessia.net>
2025-06-22 23:14:44 -0400

    Add fish shell

--- home.nix
+++ home.nix
@@ -37,22 +37,39 @@ in
   
   # Import programs configuration
   programs = import ./programs.nix { inherit pkgs username; };
-# 
-#   home.activation.chezmoi = lib.hm.dag.entryAfter ["installPackages"] ''
-#     PATH="${pkgs.chezmoi}/bin:${pkgs.git}/bin:${pkgs.git-lfs}/bin:''${PATH}"
-# 
-#     $DRY_RUN_CMD chezmoi init git.sapphicco.de/SapphicCode/dotfiles
-#     $DRY_RUN_CMD chezmoi update -a
-#     $DRY_RUN_CMD chezmoi git status
-#   '';
+ 
+   home.activation.chezmoi = lib.hm.dag.entryAfter ["installPackages" "generateSshKey"] ''
+     PATH="${pkgs.chezmoi}/bin:${pkgs.git}/bin:${pkgs.git-lfs}/bin:${pkgs.openssh}/bin:''${PATH}"
+ 
+     $DRY_RUN_CMD chezmoi init git@github.com:MichaelVessia/dots.git
+     $DRY_RUN_CMD chezmoi update -a
+     $DRY_RUN_CMD chezmoi git status
+   '';
 
   # Generate SSH key if it doesn't exist
   home.activation.generateSshKey = lib.hm.dag.entryAfter ["writeBoundary"] ''
     if [ ! -f "$HOME/.ssh/id_ed25519" ]; then
       $DRY_RUN_CMD mkdir -p $HOME/.ssh
+      $DRY_RUN_CMD chmod 700 $HOME/.ssh
       $DRY_RUN_CMD ${pkgs.openssh}/bin/ssh-keygen -t ed25519 -C "michael@vessia.net" -f $HOME/.ssh/id_ed25519 -N ""
       $DRY_RUN_CMD echo "SSH key generated! Add this public key to GitHub:"
       $DRY_RUN_CMD cat $HOME/.ssh/id_ed25519.pub
     fi
+    
+    # Ensure proper SSH permissions
+    if [ -d "$HOME/.ssh" ]; then
+      $DRY_RUN_CMD chmod 700 $HOME/.ssh
+      $DRY_RUN_CMD chmod 600 $HOME/.ssh/id_ed25519 2>/dev/null || true
+      $DRY_RUN_CMD chmod 644 $HOME/.ssh/id_ed25519.pub 2>/dev/null || true
+      $DRY_RUN_CMD chmod 600 $HOME/.ssh/config 2>/dev/null || true
+    fi
   '';
+
+  # Auto-start fish from bash
+home.file.".bashrc".text = lib.mkAfter ''
+  if [[ $(ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+  then
+    exec fish
+  fi
+'';
 }
--- programs.nix
+++ programs.nix
@@ -29,4 +29,133 @@
     vimAlias = true;
     vimdiffAlias = true;
   };
+
+  fish = {
+    enable = true;
+    
+    # Interactive shell init
+    interactiveShellInit = ''
+      # Disable greeting
+      set -g fish_greeting
+      
+      # Better colors for ls
+      set -gx LS_COLORS (${pkgs.vivid}/bin/vivid generate molokai)
+      
+      # Set your preferred editor (already set in sessionVariables but good to have here too)
+      set -gx EDITOR nvim
+      
+      # Better command not found handling
+      function fish_command_not_found
+        ${pkgs.nix-index}/bin/nix-locate --minimal --no-group --type x --type s --top-level --whole-name --at-root "/bin/$argv[1]"
+      end
+    '';
+    
+    # Shell aliases
+    shellAliases = {
+      # Nix aliases
+      rebuild = "home-manager switch --flake .#${username}";
+      update = "nix flake update";
+      
+      # Git aliases
+      g = "git";
+      gs = "git status";
+      ga = "git add";
+      gc = "git commit";
+      gp = "git push";
+      gl = "git log --oneline --graph --decorate";
+      
+      # Common shortcuts
+      ll = "ls -lah";
+      la = "ls -A";
+      l = "ls -CF";
+      
+      # Directory navigation
+      ".." = "cd ..";
+      "..." = "cd ../..";
+      "...." = "cd ../../..";
+      
+      # Safety aliases
+      rm = "rm -i";
+      cp = "cp -i";
+      mv = "mv -i";
+    };
+    
+    # Custom functions
+    functions = {
+      # Create and enter directory
+      mkcd = ''
+        mkdir -p $argv[1]
+        cd $argv[1]
+      '';
+      
+      # Extract archives
+      extract = ''
+        if test -f $argv[1]
+          switch $argv[1]
+            case '*.tar.bz2'
+              tar xjf $argv[1]
+            case '*.tar.gz'
+              tar xzf $argv[1]
+            case '*.bz2'
+              bunzip2 $argv[1]
+            case '*.gz'
+              gunzip $argv[1]
+            case '*.tar'
+              tar xf $argv[1]
+            case '*.zip'
+              unzip $argv[1]
+            case '*.7z'
+              7z x $argv[1]
+            case '*'
+              echo "Unknown archive format"
+          end
+        else
+          echo "'$argv[1]' is not a valid file"
+        end
+      '';
+      
+      # Quick backup
+      backup = ''
+        cp -r $argv[1] $argv[1].backup.(date +%Y%m%d_%H%M%S)
+      '';
+    };
+    
+    # Plugins using fishPlugins
+    plugins = [
+      {
+        name = "tide";
+        src = pkgs.fishPlugins.tide.src;
+      }
+      {
+        name = "fzf-fish";
+        src = pkgs.fishPlugins.fzf-fish.src;
+      }
+      {
+        name = "autopair";
+        src = pkgs.fishPlugins.autopair.src;
+      }
+      {
+        name = "done";
+        src = pkgs.fishPlugins.done.src;
+      }
+    ];
+  };
+  
+  # Enable starship prompt (alternative to tide - choose one)
+  # starship = {
+  #   enable = true;
+  #   enableFishIntegration = true;
+  # };
+  
+  # Other programs that integrate well with fish
+  fzf = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+  
+  zoxide = {
+    enable = true;
+    enableFishIntegration = true;
+    options = [ "--cmd cd" ];
+  };
 }
