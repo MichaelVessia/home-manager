5cf16fb00882f5aeceaefdc64f2c12e25c27f5fb
Michael Vessia <michael@vessia.net>
2025-07-29 19:12:55 -0400

    Try changing defaults usage

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -7,28 +7,28 @@
   home.activation.macosDefaults = lib.hm.dag.entryAfter ["writeBoundary"] ''
     if [[ "$(uname)" == "Darwin" ]]; then
       # Show battery percentage
-      $DRY_RUN_CMD defaults write com.apple.menuextra.battery ShowPercent -bool true
+      $DRY_RUN_CMD /usr/bin/defaults write com.apple.menuextra.battery ShowPercent -bool true
       
       # Disable natural scrolling (set to true for default macOS behavior)
-      $DRY_RUN_CMD defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+      $DRY_RUN_CMD /usr/bin/defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
       
       # Enable tap to click
-      $DRY_RUN_CMD defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-      $DRY_RUN_CMD defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+      $DRY_RUN_CMD /usr/bin/defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+      $DRY_RUN_CMD /usr/bin/defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
       
       # Show all filename extensions
-      $DRY_RUN_CMD defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+      $DRY_RUN_CMD /usr/bin/defaults write NSGlobalDomain AppleShowAllExtensions -bool true
       
       # Disable auto-correct
-      $DRY_RUN_CMD defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+      $DRY_RUN_CMD /usr/bin/defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
       
       # Set a fast key repeat rate
-      $DRY_RUN_CMD defaults write NSGlobalDomain KeyRepeat -int 2
-      $DRY_RUN_CMD defaults write NSGlobalDomain InitialKeyRepeat -int 15
+      $DRY_RUN_CMD /usr/bin/defaults write NSGlobalDomain KeyRepeat -int 2
+      $DRY_RUN_CMD /usr/bin/defaults write NSGlobalDomain InitialKeyRepeat -int 15
       
       # Restart affected apps
-      $DRY_RUN_CMD killall Finder || true
-      $DRY_RUN_CMD killall SystemUIServer || true
+      $DRY_RUN_CMD /usr/bin/killall Finder || true
+      $DRY_RUN_CMD /usr/bin/killall SystemUIServer || true
     fi
   '';
   
