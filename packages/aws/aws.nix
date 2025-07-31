36dfc105864f4d1b6a36126ea177312270e0026a
michaelvessia <michael@vessia.net>
2025-07-31 00:47:53 -0400

    openlens note

--- packages/aws/aws.nix
+++ packages/aws/aws.nix
@@ -5,4 +5,7 @@
     aws-sso-cli
     awscli2
   ];
+
+  # NOTE: For GUI applications (like OpenLens) to find AWS CLI, create a system-wide symlink:
+  # sudo ln -sf /Users/michael.vessia/.nix-profile/bin/aws /usr/local/bin/aws
 }
